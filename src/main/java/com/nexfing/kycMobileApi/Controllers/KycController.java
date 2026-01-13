2073a1450cb688131304b412ee64ef89f8b9ad05
tarik <mabcde42@gmail.com>
2026-01-13 11:13:56 +0100

    ocr

--- src/main/java/com/nexfing/kycMobileApi/Controllers/KycController.java
+++ src/main/java/com/nexfing/kycMobileApi/Controllers/KycController.java
@@ -17,7 +17,7 @@ public class KycController {
     private final KycServiceI kycService;
     private final SubmissionService submissionService;
 
-    @PostMapping("/ocr")
+    @PostMapping("/ocr2")
     public Mono<IdCardFields> extractFields(@RequestParam("image") MultipartFile file) {
         return kycService.extractFields(file);
     }
